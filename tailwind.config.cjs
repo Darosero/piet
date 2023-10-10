e76267a179ce5bf6c759fcb30a2a1bbee8d49080
climacot <fermt111@gmail.com>
2023-10-10 13:42:40 -0500

    add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#e8ebf0",
+          100: "#b7c2d1",
+          200: "#95a5bb",
+          300: "#647c9c",
+          400: "#466289",
+          500: "#183b6b",
+          600: "#163661",
+          700: "#112a4c",
+          800: "#0d203b",
+          900: "#0a192d",
+        },
+        secondary: {
+          50: "#f4e8e8",
+          100: "#dcb9b8",
+          200: "#cb9796",
+          300: "#b36866",
+          400: "#a54a49",
+          500: "#8e1d1b",
+          600: "#811a19",
+          700: "#651513",
+          800: "#4e100f",
+          900: "#3c0c0b",
+        },
+      },
+    },
+  },
+};
