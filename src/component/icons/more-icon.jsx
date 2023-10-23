5a9c58b3e7f8fdda7ea5187a112f4cbb25bd6a78
climacot <fermt111@gmail.com>
2023-10-23 11:20:28 -0500

    some refactors

--- src/component/icons/more-icon.jsx
+++ src/component/icons/more-icon.jsx
@@ -1,33 +1,9 @@
-export default function MoreIcon() {
+export default function MoreIcon(props) {
   return (
-    <svg  width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <g id="Complete">
-        <g data-name="add" id="add-2">
-          <g>
-            <line 
-        
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              x1="12"
-              x2="12"
-              y1="19"
-              y2="5"
-            ></line>
-
-            <line
-     
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              x1="5"
-              x2="19"
-              y1="12"
-              y2="12"
-            ></line>
-          </g>
-        </g>
+    <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" {...props}>
+      <g data-name="add">
+        <path d="M12 19V5M5 12h14" />
       </g>
     </svg>
-  )
+  );
 }
