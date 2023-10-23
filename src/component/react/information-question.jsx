fb5d00819e468877d72d9348ddc9b107d1fe9c4c
Darosero <darosero@unicauca.edu.co>
2023-10-22 21:22:11 -0500

    fix questions page

--- src/component/react/information-question.jsx
+++ src/component/react/information-question.jsx
@@ -8,14 +8,14 @@ export default function InformationQuestion({ title, children }) {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
-    <div className="py-4 w-[1100px]">
+    <div className="py-4 lg:w-[1100px]">
       <div className="hover:rounded-md hover:text-white hover:bg-primary-400 hover:stroke-white stroke-primary-500">
         <button
           className=" flex  border-4 border-primary-400 rounded-md w-full p-2 gap-4"
           onClick={() => setIsVisible(!isVisible)}
         >
           {!isVisible ? <MoreIcon /> : <MinusIcon />}
-          <h2 className="flex-1 text-left font-semibold text-lg ">{title}</h2>
+          <h2 className="flex-1 text-left font-semibold sm:text-base lg:text-lg ">{title}</h2>
           {!isVisible ? <ArrowRigthIcon /> : <ArrowDownIcon />}
         </button>
       </div>
