3994a1fc7ec00b9bb9d74b2e9f8bf58626f04d29
Darosero <darosero@unicauca.edu.co>
2023-11-01 20:47:49 -0500

    add goalds component and fix index

---
+++ src/component/react/goalds.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import AddIcon from "../icons/add-icon.jsx";
+
+export default function Goalds({ title, children }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <button
+        className="grid grid-rows-card justify-center border-4 border-primary-500 rounded-lg bg-primary-500 p-4"
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        <h2 className="font-bold lg:text-3xl text-white">{title}</h2>
+        <div className="flex justify-center stroke-white">
+          <AddIcon />
+        </div>
+      </button>
+      {isVisible ? <p className="p-4 border-4 rounded-lg border-primary-500/20">{children}</p> : null}
+    </div>
+  );
+}
