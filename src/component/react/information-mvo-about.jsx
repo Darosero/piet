30636fd75dbba083bbffdfa066fa1de736cc6d45
Darosero <darosero@unicauca.edu.co>
2023-11-08 21:23:54 -0500

    fix mission and vision on the program about page

--- src/component/react/information-mvo-about.jsx
+++ src/component/react/information-mvo-about.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import AddIcon from "../icons/add-icon.jsx";
 import clsx from "clsx";
+import AddIcon from "../icons/add-icon";
 
 export default function InformationMVO({ title, children, bgColor }) {
   const [isVisible, setIsVisible] = useState(false);
@@ -8,12 +8,15 @@ export default function InformationMVO({ title, children, bgColor }) {
   return (
     <button className={clsx("relative", bgColor)} onClick={() => setIsVisible(!isVisible)}>
       <div className="container group flex flex-col items-center justify-center p-4 ">
-        <div className="opacity-100 group-hover:opacity-0 font-bold text-3xl absolute stroke-white flex flex-col items-center">
+        <div className="opacity-100 group-hover:opacity-0 font-bold lg:text-3xl absolute stroke-white flex flex-col items-center">
           <h3 className="text-white"> {title} </h3>
-          <AddIcon />
+          <div className="lg:hidden sm:visible">
+          <AddIcon></AddIcon>
+          </div>
+          
         </div>
         <div className="opacity-0 group-hover:opacity-100 ">
-          <h3 className="font-bold text-xl text-white">{title}</h3>
+          <h3 className="font-bold lg:text-xl text-white">{title}</h3>
           <p className="p-2 text-white font-normal text-base hover:opacity-100">{children}</p>
         </div>
       </div>
