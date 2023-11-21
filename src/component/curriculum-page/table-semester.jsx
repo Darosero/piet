5d74e2eb0594d0d2b069384d04e945bcf0ac0093
Darosero <darosero@unicauca.edu.co>
2023-11-20 22:35:24 -0500

    fix old pensum in table semester

--- src/component/curriculum-page/table-semester.jsx
+++ src/component/curriculum-page/table-semester.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { OLDPENSUM } from "../lib/old-pensum";
 import { NEWPENSUM } from "../lib/new-pensum";
 
-function Table({ state }) {
+function Table() {
   const [hash, setHash] = useState(window.location.hash);
   const pensum = OLDPENSUM[hash] ?? OLDPENSUM["#I"];
   const newPensum = NEWPENSUM[hash] ?? NEWPENSUM["#I"];
@@ -16,7 +16,7 @@ function Table({ state }) {
   }, []);
 
   return (
-    <div style={{ height: "350px" }} className=" border-4 rounded-lg border-primary-300">
+    <div  className=" lg:h-[360px] sm:h-96 border-4 rounded-lg border-primary-300">
       <nav className=" flex bg-primary-100   text-primary-500 md:font-bold font-semibold rounded-t">
         <a href="#I" className="hover:bg-primary-200 lg:px-4 lg:py-2 p-2">
           I
@@ -45,18 +45,18 @@ function Table({ state }) {
         <a href="#IX" className="hover:bg-primary-200 lg:px-4 lg:py-2 p-2">
           IX
         </a>
-        <a href="#X" className="hover:bg-primary-200 lg:px-4 lg:py-2 p-2">
+        <a href="#X" className="hover:bg-primary-200 lg:px-4 lg:py-2 p-2 ">
           X
         </a>
       </nav>
-      <section className="grid grid-rows-[auto,auto,auto] gap-2 px-4">
+      <section className="grid grid-rows-[auto,auto,auto] gap-2 lg:px-4 px-0">
         <h1 className="text-2xl text-primary-500 px-2">{pensum?.name}</h1>
         <div className="grid grid-cols-2">
           <h3 className="text-lg px-2">{"Materias: " + pensum.subject}</h3>
           <h3 className="text-lg px-2">{"Créditos: " + pensum.credit}</h3>
         </div>
-        <section className="grid lg:grid-cols-[1fr,1fr,1fr] grid-cols-[auto,auto] ">
-          <div className="flex flex-col border-4">
+        <section className="grid lg:grid-cols-3 grid-cols-2">
+          <div className="flex flex-col border-y-4 lg:border-4 sm:border-y-4">
             <h4 className="border-b-4 px-2">Asignatura</h4>
             {pensum?.subjects.map((subject, index) => (
               <p className="px-2 " key={index}>
@@ -64,9 +64,9 @@ function Table({ state }) {
               </p>
             ))}
           </div>
-          <div className="flex flex-col border-y-4 sm:boder-r-4">
-            <h4 className="border-b-4 px-2">Créditos</h4>
+          <div className="flex flex-col border-y-4  ">
             <div>
+              <h4 className="border-b-4 px-2">Creditos</h4>
               {pensum?.credits.map((credits, index) => (
                 <p className=" px-2" key={index}>
                   {credits.name}
