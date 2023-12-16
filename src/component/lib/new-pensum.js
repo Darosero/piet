b2b40e3ad9beeb061d185e15f11022242fb10cdb
Darosero <darosero@unicauca.edu.co>
2023-12-16 17:21:10 -0500

    add new pensum

--- src/component/lib/new-pensum.js
+++ src/component/lib/new-pensum.js
@@ -111,4 +111,503 @@ export const NEWPENSUM = {
       },
     ],
   },
+  "#III": {
+    name: "Semestre III",
+    subject: "6",
+    credit: "18",
+    subjects: [
+      {
+        name: "Ecuaciones Diferenciales Ordinarias",
+      },
+      {
+        name: "Cálculo Vectorial ",
+      },
+      {
+        name: "Electromagnetismo",
+      },
+      {
+        name: "Circuitos de Corriente Alterna",
+      },
+      {
+        name: "Circuitos Electrónicos",
+      },
+      {
+        name: "Circuitos Digitales",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Cálculo integral",
+      },
+      {
+        name: "Cálculo integral",
+      },
+      {
+        name: "Mecánica",
+      },
+      {
+        name: "Circuitos de corriente directa",
+      },
+      {
+        name: "Circuitos de corriente directa",
+      },
+      {
+        name: "Programación orientada a objetos",
+      },
+    ],
+  },
+  "#IV": {
+    name: "Semestre IV",
+    subject: "6",
+    credit: "18",
+    subjects: [
+      {
+        name: "Campos",
+      },
+      {
+        name: "Señales y Sistemas",
+      },
+      {
+        name: "Algoritmos Computacionales",
+      },
+      {
+        name: "Circuitos Analógicos",
+      },
+      {
+        name: "Diseño de circuitos con VHDL",
+      },
+      {
+        name: "Vibraciones y Ondas",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Electromagnetismo",
+      },
+      {
+        name: "Ecuaciones diferenciales ordinarias",
+      },
+      {
+        name: "Programación orientada a objetos",
+      },
+      {
+        name: "Circuitos electrónicos",
+      },
+      {
+        name: "Circuitos digitales",
+      },
+      {
+        name: "Electromagnetismo",
+      },
+    ],
+  },
+  "#V": {
+    name: "Semestre V",
+    subject: "6",
+    credit: "17",
+    subjects: [
+      {
+        name: "Procesamiento Digital de Señales ",
+      },
+      {
+        name: "Probabilidad y Estadística",
+      },
+      {
+        name: "Microcontroladores",
+      },
+      {
+        name: "Medios de Transmisión",
+      },
+      {
+        name: "Fundamentos de Redes de Telecomunicaciones",
+      },
+      {
+        name: "Modelado y Bases de Datos",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "2",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Señales y Sistemas",
+      },
+      {
+        name: "Cálculo Vectorial",
+      },
+      {
+        name: "Circuitos Digitales",
+      },
+      {
+        name: "Campos",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "Algoritmos Computacionales",
+      },
+    ],
+  },
+  "#VI": {
+    name: "Semestre VI",
+    subject: "6",
+    credit: "18",
+    subjects: [
+      {
+        name: "Circuitos de RF",
+      },
+      {
+        name: "Comunicaciones Analógicas",
+      },
+      {
+        name: "Metodología de la Investigación",
+      },
+      {
+        name: "Sistemas de Conmutación",
+      },
+      {
+        name: "Comunicaciones Digitales",
+      },
+      {
+        name: "Informática para Telecomunicaciones",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Circuitos analógicos",
+      },
+      {
+        name: "Prob y E. P.D.S.",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "Fund. Redes de Telecomunicaciones",
+      },
+      {
+        name: "Prob y E., Señales y sistemas",
+      },
+      {
+        name: "Algoritmos Computacionales",
+      },
+    ],
+  },
+  "#VII": {
+    name: "Semestre VII",
+    subject: "6",
+    credit: "17",
+    subjects: [
+      {
+        name: "Sistemas Embebidos y Tiempo Real",
+      },
+      {
+        name: "Comunicaciones Móviles Inalámbricas",
+      },
+      {
+        name: "Sistemas de Comunicaciones Ópticas",
+      },
+      {
+        name: "Radiocomunicaciones",
+      },
+      {
+        name: "Servicios Convergentes",
+      },
+      {
+        name: "Emprendimiento e innovación en ingeniero",
+      },
+      {
+        name: "Anteproyecto de Grado",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "2",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Microcontroladores",
+      },
+      {
+        name: "Com. Digitales y Com. analógicas",
+      },
+      {
+        name: "Com. Digitales y Medios de Tx",
+      },
+      {
+        name: "Com. Digitales y Com. Analógicas",
+      },
+      {
+        name: "Sistemas de Conmutación",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+    ],
+  },
+  "#VIII": {
+    name: "Semestre VIII",
+    subject: "6",
+    credit: "17",
+    subjects: [
+      {
+        name: "Proyecto Integrador",
+      },
+      {
+        name: "Laboratorio I de Sistemas de Telecomunicaciones",
+      },
+      {
+        name: "Énfasis I (Área Tc, Tm)",
+      },
+      {
+        name: "Electiva I (Área Tc, Tm, Ex, Sx)",
+      },
+      {
+        name: "Electiva II (Área Tc, Tm, Ex, Sx)",
+      },
+      {
+        name: "Competencias ciudadanas",
+      },
+    ],
+    credits: [
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "2",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "Sistemas Embebidos y Tiempo Real",
+      },
+      {
+        name: "Radiocom. y Sist de Com. ópticas",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "Ética",
+      },
+    ],
+  },
+  "#IX": {
+    name: "Semestre IX",
+    subject: "6",
+    credit: "17",
+    subjects: [
+      {
+        name: "Formulación y Gestión de Proyectos en TIC",
+      },
+      {
+        name: "Laboratorio II de Sistemas de Telecomunicaciones",
+      },
+      {
+        name: "Laboratorio de Servicios Telemáticos",
+      },
+      {
+        name: "Énfasis II (Área Tc, Tm)",
+      },
+      {
+        name: "Énfasis III (Área Tc, Tm)",
+      },
+      {
+        name: "Electiva III (Área Tc, Tm, Ex, Sx)",
+      },
+    ],
+    credits: [
+      {
+        name: "2",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "3",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "___",
+      },
+      {
+        name: "Lab. I de Sist. de Telecomunicaciones",
+      },
+      {
+        name: "Servicios Convergentes",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+    ],
+  },
+  "#X": {
+    name: "Semestre X",
+    subject: "4",
+    credit: "16",
+    subjects: [
+      {
+        name: "Análisis económico de inversiones en TIC",
+      },
+      {
+        name: "Énfasis IV (Área Tc, Tm, Ex, Sx) ",
+      },
+      {
+        name: "Trabajo de Grado",
+      },
+      {
+        name: "Formación académico-tecnológica (*)",
+      },
+    ],
+    credits: [
+      {
+        name: "2",
+      },
+      {
+        name: "3",
+      },
+      {
+        name: "11",
+      },
+    ],
+    prerequisites: [
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+      {
+        name: "___",
+      },
+    ],
+  },
 };
